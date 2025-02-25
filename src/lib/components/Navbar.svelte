69fcc2346a8401739b2c4e20ce738f6e31778bd2
Sangelo <contact@sangelo.space>
2025-02-25 20:59:25 +0100

    [a] progress on initial poc

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,22 @@
+<div class="flex h-full flex-col gap-4 text-lg">
+	<a href="/">
+		<img class="w-32" alt="krchv" src="/assets/svg/logo.svg" />
+	</a>
+	<p>search...</p>
+	<div class="flex flex-col">
+		<a class="!text-base-content !no-underline hover:opacity-80" href="/">Home</a>
+		<a class="!text-base-content !no-underline hover:opacity-80" href="/">About</a>
+	</div>
+	<div>
+		<p class="text-sm uppercase opacity-50">Keyboards</p>
+		<ul>
+			<li>keyboards</li>
+			<li>keyboards</li>
+			<li>keyboards</li>
+		</ul>
+	</div>
+	<div class="mt-auto flex flex-col gap-2">
+		<p>&copy; 2025 LogolicusZ</p>
+		<a href="//logolicusz.com">&larr; logolicusz.com</a>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<svelte:head>
+	<link href="/assets/fonts/geist/geist-variable.ttf" rel="stylesheet" />
+	<link href="/assets/fonts/geist-mono/geist-mono-variable.ttf" rel="stylesheet" />
+	<link href="/assets/fonts/daydream/Daydream.ttf" rel="stylesheet" />
+</svelte:head>
+
+<main class="flex h-dvh w-full gap-12 p-12">
+	<Navbar />
+	{@render children()}
+</main>
