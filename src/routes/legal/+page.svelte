7ff0af31dd49bd09736c6eefa5fe191be9679458
Sangelo <contact@sangelo.space>
2025-07-16 14:44:23 +0200

    [c] overhaul privacy page

--- src/routes/legal/+page.svelte
+++ src/routes/legal/+page.svelte
@@ -1,75 +1,83 @@
 <svelte:head>
-  <title>Legal | krchv.logolicusz.com</title>
+  <title>Legal - krchv</title>
   <meta name="description" content="A collection of legal documents for krchv.logolicusz.com." />
 </svelte:head>
 
-<div class="min-h-screen px-4 py-12 sm:px-6 lg:px-8">
-  <div class="mx-auto max-w-3xl p-4 sm:p-6 lg:p-8">
-    <div
-      class="prose-a:underline">
-      <h2 class="border-primary/50 mt-0 mb-4 border-b pb-1 text-lg font-bold">Privacy Policy</h2>
+<div class="mx-auto max-w-3xl p-4 sm:p-6 lg:p-8">
+  <div class="prose-a:underline">
+    <!-- <h2 class="border-primary/50 mt-0 mb-4 border-b pb-1 text-lg font-bold">Privacy Policy</h2> -->
+    <h1 class="font-daydream mb-8 text-4xl">Privacy Policy</h1>
 
-      <p>
-        Welcome to my website! As someone who values privacy, I am committed to safeguarding your personal information
-        and ensuring it is handled securely and responsibly. This privacy policy explains what data is collected, how it
-        is used, and the measures taken to protect your information when you visit krchv.
-      </p>
-      <p>
-        If you have any questions or concerns about this policy, please contact me at
-        <span class="text-primary">logolicusz@gmail.com</span>
-      </p>
+    <p>
+      Welcome to krchv! As someone who values privacy, I am committed to safeguarding your personal information and
+      ensuring it is handled securely and responsibly. This privacy policy explains what data is collected, how it is
+      used, and the measures taken to protect your information when you visit krchv.
+    </p>
+    <p>
+      If you have any questions or concerns about this policy, please contact me at
+      <a href="mailto:logolicusz@gmail.com">logolicusz@gmail.com</a>
+      .
+    </p>
 
-      <h2 id="analytics" class="mt-8 text-lg font-bold">Analytics</h2>
-      <p>
-        When you visit any page on this website, a small piece of data is collected and sent to a
-        <a href="https://plausible.io" target="_blank" rel="nofollow noopener noreferrer">Plausible instance</a>
-        hosted by
-        <a href="https://lunivity.com">lunivity.com</a>
-        .
-      </p>
-      <p>It's important to note that:</p>
-      <ul class="ml-5 !list-disc sm:ml-6">
-        <li>No personal information is collected</li>
-        <li>No cookies used for analytics are stored in the browser</li>
-        <li>No information is shared with, sent to, or sold to third-parties</li>
-        <li>No information is shared with advertising companies</li>
-        <li>No information is mined or harvested for personal and behavioral trends</li>
-        <li>No information is monetized</li>
-      </ul>
-      <p>
-        I run the Plausible Analytics script to collect some anonymous usage data for statistical purposes. The goal is
-        to track overall trends in my website traffic—not individual visitors.
-        <span class="text-primary font-semibold">Absolutely no personal data is collected for analytics.</span>
-      </p>
-      <p>
-        Data collected includes referral sources, top pages, visit duration, device type, OS, country, browser, etc. See
-        <a href="https://plausible.io/data-policy" target="_blank" rel="nofollow noopener noreferrer">
-          Plausible's data policy
-        </a>
-        .
-      </p>
+    <h2 id="analytics" class="mt-8 text-lg font-bold">Analytics</h2>
+    <p>
+      When you visit any page on this website, a small piece of data is collected and sent to a
+      <a href="https://plausible.io" target="_blank" rel="nofollow noopener noreferrer">Plausible instance</a>
+      hosted by
+      <a href="https://lunivity.com">lunivity.com</a>
+      .
+    </p>
+    <p>It's important to note that:</p>
+    <ul class="ml-5 !list-disc sm:ml-6">
+      <li>No personal information is collected</li>
+      <li>No cookies used for analytics are stored in the browser</li>
+      <li>No information is shared with, sent to, or sold to third-parties</li>
+      <li>No information is shared with advertising companies</li>
+      <li>No information is mined or harvested for personal and behavioral trends</li>
+      <li>No information is monetized</li>
+    </ul>
+    <p>
+      I run the Plausible Analytics script to collect some anonymous usage data for statistical purposes. The goal is to
+      track overall trends in my website traffic, not individual visitors.
+      <span class="font-semibold">Absolutely no personal data is collected for analytics.</span>
+    </p>
+    <p>
+      Data collected includes referral sources, top pages, visit duration, device type, OS, country, browser, etc. See
+      <a href="https://plausible.io/data-policy" target="_blank" rel="nofollow noopener noreferrer">
+        Plausible's data policy
+      </a>
+      .
+    </p>
 
-      <h2 id="general-log-collection" class="mt-8 text-lg font-bold">General Log Collection</h2>
-      <p>
-        This website is hosted on GitHub Pages. As the site owner, I do <strong>not</strong>
-         collect or have access to any server logs or personal info about your visit.
-      </p>
-      <p>However, GitHub may automatically collect certain technical data such as:</p>
-      <ul class="ml-5 !list-disc sm:ml-6">
-        <li>IP addresses</li>
-        <li>Browser type and version</li>
-        <li>Referring site</li>
-        <li>Time of visit</li>
-        <li>Pages visited</li>
-        <li>Operating system</li>
-      </ul>
-      <p>
-        This data is collected and processed by GitHub. I do not have access to it. See the
-        <a href="https://docs.github.com/privacy" target="_blank" rel="noopener noreferrer">GitHub Privacy Statement</a>
-        . Questions? Email me at
-        <span class="text-primary">logolicusz@gmail.com</span>
-        .
-      </p>
-    </div>
+    <h2 id="general-log-collection" class="mt-8 text-lg font-bold">General Log Collection</h2>
+    <p>
+      This website is hosted on GitHub Pages. As the site owner, I do <strong>not</strong>
+      collect or have access to any server logs or personal info about your visit.
+    </p>
+    <p>However, GitHub may automatically collect certain technical data such as:</p>
+    <ul class="ml-5 !list-disc sm:ml-6">
+      <li>IP addresses</li>
+      <li>Browser type and version</li>
+      <li>Referring site</li>
+      <li>Time of visit</li>
+      <li>Pages visited</li>
+      <li>Operating system</li>
+    </ul>
+    <p>
+      This data is collected and processed by GitHub. I do not have access to it. See the
+      <a href="https://docs.github.com/privacy" target="_blank" rel="noopener noreferrer">GitHub Privacy Statement</a>
+      .
+      <br />
+      Questions? Email me at
+      <a href="mailto:logolicusz@gmail.com">logolicusz@gmail.com</a>
+      .
+    </p>
   </div>
 </div>
+
+<style>
+  p,
+  ul {
+    margin-bottom: 0.5rem;
+  }
+</style>
