a44ad941d52f31b0124dcede38a16ed00db054a6
LogolicusZ <adjordbra@gmail.com>
2025-07-16 17:18:09 +0200

    [c] Updated welcome Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,18 @@
   <title>Home - krchv</title>
 </svelte:head>
 
-<div class="font-daydream relative flex h-full w-full items-center justify-center text-3xl">
-  <!--<h1 class="z-10 text-white">Welcome to the Keyboard Archive!</h1>-->
+<div class="relative flex h-full w-full items-center justify-center">
   <img
-    class="relative top-0 left-0 z-0 h-full w-full object-cover p-0 blur-xs"
+    class="absolute top-0 left-0 z-0 h-full w-full object-cover p-0 blur-xs"
     src="/assets/img/banner.avif"
-    alt="banner background" />
+    alt="banner background"
+  />
+  <div class="z-10 flex flex-col items-center space-y-4 sm:space-y-5 lg:space-y-8 px-4">
+    <h1 class="font-daydream text-3xl sm:text-4xl lg:text-5xl text-white text-center">
+      Keyboard Archive
+    </h1>
+    <p class="text-base sm:text-lg md:text-xl lg:text-2xl text-white text-center max-w-xl mx-auto px-2">
+  Discover my collection and also all my keyboard pictures in one space!
+</p>
+  </div>
 </div>
