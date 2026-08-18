6adbb9b63d419b022a951a94592f49e288a91f74
LogolicusZ <adjordbra@gmail.com>
2026-08-18 16:12:29 +0200

    [a] added new effect to landing page + button for keyboards page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,29 @@
   import { fly } from "svelte/transition";
   import { cubicOut } from "svelte/easing";
   import { motionSafe } from "$lib/utils/motion.svelte";
+  import Asciify from "$lib/components/canvasui/Asciify.svelte";
 </script>
 
 <svelte:head>
   <title>Home - krchv</title>
 </svelte:head>
-
+<Asciify
+  class="-m-12 h-[calc(100%+6rem)] w-[calc(100%+6rem)] md:ml-0 md:w-[calc(100%+3rem)]"
+  radius={0.4}
+  softness={1}
+  scale={2}
+  spacing={1}
+  backgroundOpacity={0}
+  contrast={1}
+  brightness={0}
+  invert={0}
+  glow={0.75}
+  aberration={0.75}
+  strength={1}
+  baseStrength={0}
+  followSpeed={3}
+  charset="ascii"
+  background={[0, 0, 0]}>
 <div class="relative flex h-full w-full items-center justify-center">
   <img
     class="absolute top-0 left-0 z-0 h-full w-full object-cover p-0 blur-xs"
@@ -26,5 +43,17 @@
       in:fly|global={motionSafe({ y: 12, duration: 500, delay: 120, easing: cubicOut })}>
       Discover my collection and also all my keyboard pictures in one space!
     </p>
+    <a
+      href="/table/keyboards"
+      class="unset-link font-daydream bg-base-100 inline-block px-7 py-4 text-xs tracking-[0.15em] text-black uppercase no-underline
+        [clip-path:polygon(0_8px,8px_8px,8px_0,calc(100%_-_8px)_0,calc(100%_-_8px)_8px,100%_8px,100%_calc(100%_-_8px),calc(100%_-_8px)_calc(100%_-_8px),calc(100%_-_8px)_100%,8px_100%,8px_calc(100%_-_8px),0_calc(100%_-_8px))]
+        drop-shadow-[6px_6px_0_#000] transition-[transform,filter] duration-75
+        hover:translate-x-[3px] hover:translate-y-[3px] hover:drop-shadow-[3px_3px_0_#000]
+        active:translate-x-[6px] active:translate-y-[6px] active:drop-shadow-[0_0_0_#000]
+        focus-visible:drop-shadow-[3px_3px_0_#fff] focus-visible:outline-none sm:text-sm"
+      in:fly|global={motionSafe({ y: 12, duration: 500, delay: 240, easing: cubicOut })}>
+      Browse keyboards
+    </a>
   </div>
 </div>
+</Asciify>
